e6ea0576f8f1f2bd7b3def5e1740306c6056f0e2
harshali <harshalimankar8@gmail.com>
2024-09-24 17:00:13 +0530

    contact form using emailjs

--- src/contact-from.js
+++ src/contact-from.js
@@ -1,52 +1,50 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import emailjs from "@emailjs/browser";
-import { useRef } from "react";
-function Contact() {
+import { useRef, useState } from "react";
+const Contact = () => {
+  const SERVICE_ID = "service_i22eil7";
+  const TEMPLATE_ID = "template_2v7h5ds";
+  const PUBLIC_KEY = "Z1m-Rmd5kQj3P-Sxl";
+
   const form = useRef();
+
   const handleSubmitEmail = async (e) => {
-    debugger;
     e.preventDefault();
-    console.log("in submit");
-    emailjs
-      .sendForm(
-        "service_deupqn9",
-        "template_2v7h5ds",
-        form.current,
-        "Z1m-Rmd5kQj3P-Sxl"
-      )
-      .then(
-        (result) => {
-          debugger;
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log(result.text);
+        e.target.reset();
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
   return (
-    <Form>
+    <Form ref={form} onSubmit={handleSubmitEmail} className="mb-3">
       <Form.Group
         ref={form}
         onSubmit={handleSubmitEmail}
         className="mb-3"
-        controlId="harshalimankar8@gmail.com"
+        //controlId="harshalimankar8@gmail.com"
       >
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Name" />
+        <Form.Control type="text" placeholder="Name" name="to_name" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Email address</Form.Label>
         <Form.Control
           type="email"
-          name={"user_email"}
+          name="user_email"
           placeholder="Enter email"
         />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Message</Form.Label>
         <Form.Control
+          name="message"
           as="textarea"
           placeholder="Describe your project"
           rows={3}
@@ -58,6 +56,6 @@ function Contact() {
       </Button>
     </Form>
   );
-}
+};
 
 export default Contact;
